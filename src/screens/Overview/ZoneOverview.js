25e6e3d450e24bea95bf6dcb79364da4cffe9fa1
keerthanak17 <43840331+keerthanak17@users.noreply.github.com>
2020-04-14 09:57:14 +0200

    Update ZoneOverview.js

--- src/screens/Overview/ZoneOverview.js
+++ src/screens/Overview/ZoneOverview.js
@@ -5,7 +5,7 @@ import Schedule from "./Schedule";
 const ZoneOverview = (props) => {
   return (
     <div className="ZoneOverview">
-      <header>Zone Schedules List : </header>
+      <header>Schedules List : </header>
       {props.schedulesList.map((schedule, key) => (
         <Schedule
           ScheduleId={schedule.schedule_id}
